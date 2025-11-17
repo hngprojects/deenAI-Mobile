3626f79895eaab00e9bf850075a15f164d4268f7
Umar Sulaiman <akoredeumar8@gmail.com>
2025-11-17 18:44:32 +0100

    feat: Hadith collections & Hadith categories screen

---
+++ app/(hadiths-collection)/(id)/hadith-categories.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+const categories = [
+  { id: 1, title: "Revelation", range: "1-7" },
+  { id: 2, title: "Belief", range: "8-85" },
+  { id: 3, title: "Knowledge", range: "59-134" },
+  { id: 4, title: "Ablution (Wudu’)", range: "135-248" },
+  { id: 5, title: "Bathing (Ghusl)", range: "249-293" },
+  { id: 6, title: "Menstrual Periods", range: "294-330" },
+  { id: 7, title: "Prayers (Solat)", range: "331-345" },
+  { id: 8, title: "Menstrual Periods", range: "346-512" },
+  { id: 9, title: "Times of the prayers", range: "513-590" },
+  { id: 10, title: "Call to Prayers", range: "591-856" },
+  { id: 11, title: "Friday Prayer", range: "294-330" },
+  { id: 12, title: "Menstrual Periods", range: "294-330" },
+];
+
+const HadithCategories = () => {
+  return (
+    <ScrollView style={styles.container}>
+      <Text style={styles.header}>List</Text>
+
+      {categories.map((item) => (
+        <View key={item.id} style={styles.row}>
+          <View style={styles.badge}>
+            <Text style={styles.badgeText}>{item.id}</Text>
+          </View>
+
+          <Text style={styles.title}>{item.title}</Text>
+
+          <Text style={styles.range}>{item.range}</Text>
+        </View>
+      ))}
+    </ScrollView>
+  );
+};
+
+export default HadithCategories;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 16,
+    paddingTop: 20,
+    backgroundColor: "#f7f7f7",
+    flex: 1,
+  },
+  header: {
+    fontSize: 22,
+    fontWeight: "600",
+    marginBottom: 20,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "white",
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderRadius: 12,
+    marginBottom: 10,
+  },
+  badge: {
+    width: 32,
+    height: 32,
+    borderRadius: 8,
+    backgroundColor: "#F1F1F1",
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 12,
+  },
+  badgeText: {
+    fontWeight: "600",
+    fontSize: 15,
+  },
+  title: {
+    flex: 1,
+    fontSize: 15,
+    color: "#222",
+  },
+  range: {
+    fontSize: 14,
+    color: "#777",
+    fontWeight: "500",
+  },
+});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,20 @@
-import { ThemeProvider } from '@/context/ThemeContext';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
+import { ThemeProvider } from "@/context/ThemeContext";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
-    'NunitoSans-SemiBold': require('../assets/fonts/NunitoSans-SemiBold.ttf'),
-    'NunitoSans-Bold': require('../assets/fonts/NunitoSans-Bold.ttf'),
-    'NunitoSans-Regular': require('../assets/fonts/NunitoSans-Regular.ttf'),
-    'NunitoSans-Light': require('../assets/fonts/NunitoSans-Light.ttf'),
-    'NunitoSans-ExtraBold': require('../assets/fonts/NunitoSans-ExtraBold.ttf'),
-    'NunitoSans-Black': require('../assets/fonts/NunitoSans-Black.ttf'),
+    "NunitoSans-SemiBold": require("../assets/fonts/NunitoSans-SemiBold.ttf"),
+    "NunitoSans-Bold": require("../assets/fonts/NunitoSans-Bold.ttf"),
+    "NunitoSans-Regular": require("../assets/fonts/NunitoSans-Regular.ttf"),
+    "NunitoSans-Light": require("../assets/fonts/NunitoSans-Light.ttf"),
+    "NunitoSans-ExtraBold": require("../assets/fonts/NunitoSans-ExtraBold.ttf"),
+    "NunitoSans-Black": require("../assets/fonts/NunitoSans-Black.ttf"),
   });
 
   useEffect(() => {
@@ -33,6 +33,10 @@ export default function RootLayout() {
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" />
         <Stack.Screen name="(auth)" />
+        <Stack.Screen
+          name="(hadiths-collection)"
+          options={{ title: "Hadiths Collections" }}
+        />
         {/* <Stack.Screen name="(tabs)" /> */}
       </Stack>
     </ThemeProvider>
