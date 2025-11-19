87d9927995117d4a38607b01fe0f745141a4be84
Umar Sulaiman <akoredeumar8@gmail.com>
2025-11-19 12:53:55 +0100

    feat: Hadiths collection screen

---
+++ app/(hadiths-collection)/_layout.tsx
@@ -0,0 +1,16 @@
+import { Stack } from "expo-router";
+import React from "react";
+
+const HadithCollectionLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="hadiths" options={{ title: "Hadiths Collection" }} />
+      {/* <Stack.Screen
+        name="(id/hadith-categories)"
+        options={{ headerShown: false }}
+      /> */}
+    </Stack>
+  );
+};
+
+export default HadithCollectionLayout;
